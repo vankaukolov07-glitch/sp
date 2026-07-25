eda575bda6ba4993276d59f63d43d1e2aef44db9
vankaukolov07-glitch <vankaukolov07@gmail.com>
2026-07-26 01:39:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ async def handle_photo_submission(message: types.Message):
     
     # Настройки шрифта (можно увеличить размер, так как текст по центру)
     try:
-        font = ImageFont.truetype("arial.ttf", size=280) 
+        font = ImageFont.truetype("ArialBlack.ttf", size=100) 
     except IOError:
         font = ImageFont.load_default()
         
